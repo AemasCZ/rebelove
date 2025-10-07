39996328a747bee3f29f25412b64ecb734410c6c
AemasCZ <159318830+AemasCZ@users.noreply.github.com>
2025-10-07 10:11:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -296,7 +296,7 @@ player_images = {
      'žanet': "https://i.imgur.com/AwJm34d.jpeg",
      'lenka': "https://i.imgur.com/DXeR3UY.jpeg",
      'jakub': "https://i.imgur.com/KZF9Xjt.jpeg",
-     'patrik': "https://i.imgur.com/KZF9Xjt.jpeg",
+     'patrik': "https://i.imgur.com/0e6W2zA.jpeg",
 }
 
 # Výpočet dat pro tabulku
