fcb23b9428dfc791da98dd1fd3dba3d113bb5786
AemasCZ <159318830+AemasCZ@users.noreply.github.com>
2026-01-31 20:32:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -254,12 +254,12 @@ st.markdown("""
     div[data-testid="stExpander"] > details > summary svg {
         color: white !important;
     }
-    /* Třetí expander - červený nadpis */
-    div[data-testid="stExpander"] + div[data-testid="stExpander"] + div[data-testid="stExpander"] > details > summary {
+    /* Červený expander */
+    .danger-expander div[data-testid="stExpander"] > details > summary {
         background-color: #e00000 !important;
         color: white !important;
     }
-    div[data-testid="stExpander"] + div[data-testid="stExpander"] + div[data-testid="stExpander"] > details > summary svg {
+    .danger-expander div[data-testid="stExpander"] > details > summary svg {
         color: white !important;
     }
 
